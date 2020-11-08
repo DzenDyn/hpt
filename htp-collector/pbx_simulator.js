fdf358a018d7f711d08821dc1bf472015d7cccf0
dzendyn <dzendyn@gmail.com>
2020-11-08 15:32:08 +0300

    realized pbx_simulator

--- htp-collector/pbx_simulator.js
+++ htp-collector/pbx_simulator.js
@@ -1,16 +1,70 @@
 import { createServer } from 'net';
+import dt from 'date-and-time';
 
-const getRandomRange = (min, max) => {
+// configure there
+const PORT = 1337;
+const HOST = 'localhost';
+
+const getRandom = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-const generateSubscriber = () => {};
+const generateNumericString = (minLength, maxLength) => {
+    let phone = '';
+    for (let i = 0; i < getRandom(minLength, maxLength); i++) {
+        phone += getRandom(0, 9);
+    }
 
+    if (phone.length < maxLength) {
+        const needSpaces = maxLength - phone.length;
+        for (let j = 0; j < needSpaces; j++) {
+            phone = ` ${phone}`;
+        }
+    }
+    return phone;
+};
+
+const subscriber = () => {
+    return generateNumericString(4, 10);
+};
+
+const external = () => {
+    return generateNumericString(7, 22);
+};
+
+const trunk = () => {
+    return generateNumericString(1, 3);
+};
+
+const direction = () => {
+    return getRandom(0, 1);
+};
+
+const exitCode = () => {
+    return getRandom(0, 9);
+};
+
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+        currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+}
+
+/* eslint no-constant-condition: ["error", { "checkLoops": false }] */
 const server = createServer((socket) => {
-    socket.write('Test server \r\n');
-    socket.pipe(socket);
+    while (true) {
+        const now = new Date();
+        const date = dt.format(now, 'YYYY-MM-DD HH:mm:ss');
+        const newDate = new Date(now.getTime() + getRandom(1, 1800) * 1000 - 3600 * 3 * 1000);
+        const duration = dt.format(new Date(newDate - now), 'HH:mm:ss');
+        socket.write(
+            `${date} ${duration} ${subscriber()} ${external()} ${trunk()} ${direction()} ${exitCode()}\r\n`
+        );
+        socket.pipe(socket);
+        sleep(getRandom(1, 5) * 1000);
+    }
 });
 
-// server.listen(1337, 'localhost');
-
-console.log(getRandomRange(1, 9));
+server.listen(PORT, HOST);
